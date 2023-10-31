f27a73387ab1e13d3af390be7b6c0a17bc42ff40
AmbatiSaiSindhur <143985431+AmbatiSaiSindhur@users.noreply.github.com>
2023-10-31 09:43:43 +0530

    Add files via upload

---
+++ lab 6.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import matplotlib.pyplot as plt
+
+# Sample data (replace with your actual data)
+x = [1, 2, 3, 4, 5]  # Values for the first feature
+y = [10, 12, 8, 15, 20]  # Values for the second feature
+
+# Create a scatter plot
+plt.scatter(x, y)
+
+# Add labels and a title
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+plt.title("Scatter Plot of Feature 1 vs. Feature 2")
+
+# Show the plot
+plt.show()
+
+
+# In[ ]:
+
+
+
+
