8b606d9351867abf0634d040171c56c4e04aa91f
AmbatiSaiSindhur <ambatisaisindhur.com>
2023-11-06 08:55:54 +0530

    first upload

---
+++ lab 7.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn import svm
+
+# Step 1: Import the necessary libraries
+
+# Step 2: Create an SVM classifier object
+clf = svm.SVC()
+
+# Step 3: Train the SVM classifier on your training data (replace X_train and y_train)
+X_train = ...  # Your training data (2D array-like)
+y_train = ...  # Corresponding class labels (1D array or list)
+
+clf.fit(X_train, y_train)
+
+# Step 4: Retrieve the support vectors
+support_vectors = clf.support_vectors_
+
+# You can now study and analyze the support vectors as needed.
